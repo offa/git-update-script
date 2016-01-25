0c6c63e121df9840a880fa109fe5afe37b49c6b2
offa <bm-dev@yandex.com>
2016-01-25 21:25:48 +0100

    Cleanup.

--- update.sh
+++ update.sh
@@ -10,8 +10,7 @@ function git_update()
     if [ -d "$1/.git/" ]
     then
         cd $1;
-        OUTPUT="$(git fetch --all --prune)"
-        echo -e "\e[1;32m =====> $1 \e[0m\n${OUTPUT}\n"
+        echo -e "\e[1;32m =====> $1 \e[0m\n$(git fetch --all --prune)\n"
         cd $OLDPWD
     else
         echo -e "skipping $1 - .git/ not found\n"
