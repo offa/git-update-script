dccd3beff90c702d74e17bfcb2efe723751eb171
offa <bm-dev@yandex.com>
2016-01-25 20:46:47 +0100

    Temp files removed.

--- update.sh
+++ update.sh
@@ -1,17 +1,17 @@
 #!/bin/bash
 
-USH_CNT=0
-USH_RAND=$RANDOM$RANDOM
-
 
 shopt -s nullglob
+
+echo -e "\e[1;31m Start fetching all Git projects in '${PWD}' \e[0m\n"
 
 function git_update()
 {
     if [ -d "$1/.git/" ]
     then
-        echo "fetching $1"
-        cd $1; git fetch --all --prune
+        cd $1;
+        OUTPUT="$(git fetch --all --prune)"
+        echo -e "\e[1;32m =====> $1 \e[0m\n${OUTPUT}\n"
         cd $OLDPWD
     else
         echo "skipping $1 - .git/ not found"
@@ -20,19 +20,11 @@ function git_update()
 
 for dir in *; do
     (
-        if [ -d "${dir}"  ]
-        then
-            git_update ${dir} > $USH_RAND$USH_CNT.txt 2>&1
-        fi
-    ) &
-    if [ -d "${dir}" ]; then
-        ((USH_CNT++))
+    if [ -d "${dir}"  ]
+    then
+        git_update ${dir}
     fi
+    ) &
 done
 
 wait
-
-for ((i=0; i<$USH_CNT;i++)); do
-    cat $USH_RAND$i.txt 
-    rm $USH_RAND$i.txt
-done
