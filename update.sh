52a633d26a479b5eeae121ad89fa9fc156474501
offa <bm-dev@yandex.com>
2017-04-07 17:41:35 +0200

    Fix formatting for ignored directories.

--- update.sh
+++ update.sh
@@ -7,14 +7,15 @@ echo -e "\e[1;31m Start fetching all Git projects in '${PWD}' \e[0m\n"
 
 function git_update()
 {
+    echo -e "\n\e[1;32m =====> $1 \e[0m"
+
     if [ -d "$1/.git/" ]
     then
         cd $1;
-        echo -e "\n\e[1;32m =====> $1 \e[0m"
         git fetch --all --prune
         cd $OLDPWD
     else
-        echo -e "skipping $1 - .git/ not found\n"
+        echo -e "skipping $1 - .git/ not found"
     fi
 }
 
